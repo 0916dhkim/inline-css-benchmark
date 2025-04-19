9f9d27e6f0deaff02b8aff433fb131f2b79c6961
Danny Kim <0916dhkim@gmail.com>
2025-04-19 00:50:43 -0400

    Improve StyleSheet typing

--- src/inline/stylesheet.tsx
+++ src/inline/stylesheet.tsx
@@ -1,13 +1,14 @@
 import { camelToKebab } from "../camel-to-kebab";
 
-export interface StyleSheet {
-  [key: string]: string | StyleSheet;
-}
+export type StyleSheet =
+  | React.CSSProperties
+  | {
+      [selector: string]: StyleSheet;
+    };
 
 export const styleToString = (style: StyleSheet) => {
-  return Object.keys(style)
-    .reduce((acc: string[], key) => {
-      const value = style[key];
+  return Object.entries(style)
+    .reduce((acc: string[], [key, value]) => {
       const normalizedKey = camelToKebab(key);
       let rule: string;
       if (typeof value === "string") {
