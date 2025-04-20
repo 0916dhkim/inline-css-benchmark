90e5e20504274fa27b7c1ca8406e3e7e233b3bd0
Danny Kim <0916dhkim@gmail.com>
2025-04-19 23:39:11 -0400

    Use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./app.tsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
