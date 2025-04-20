ed7fb95a6670c296a4b01a6116ddc8c561748e7d
Danny Kim <0916dhkim@gmail.com>
2025-04-19 23:14:39 -0400

    Add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["styled-components"],
-      },
-    }),
-    tailwindcss(),
-  ],
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [
+      react({
+        babel: {
+          plugins: ["styled-components"],
+        },
+      }),
+      tailwindcss(),
+    ],
+  };
 });
