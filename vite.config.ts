9d312fb75750ec090efe50ace7f1b1559e085b5f
Danny Kim <0916dhkim@gmail.com>
2025-04-19 23:18:39 -0400

    Use process.env in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
     base: env.VITE_BASE_PATH,
     plugins: [
